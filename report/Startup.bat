928e2420114e36c769e8995068278e24a571472f
LUFT-AQUILA <luftaquila@protonmail.ch>
2019-08-29 16:00:31 +0900

    Add files via upload

---
+++ report/Startup.bat
@@ -0,0 +1,7 @@
+cd C:\Users\PC12\Dropbox
+forfiles /D -14 /C "cmd /c rmdir /s /q @file"
+start https://docs.google.com/spreadsheets/d/1vMeYMA-GLuTP_o0pUCwe9oUxSEoXZIXseeiYiiARObg/edit#gid=0
+start https://luftaquila.github.io/ajoumaker/
+D:
+cd 메이커스페이스
+node server
