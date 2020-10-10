6dfd0a62411ad09e66bee9f7061380abca17c610
luftaquila <mail@luftaquila.io>
2020-10-10 23:17:56 +0900

    v3.0

--- report/Startup.bat
+++
@@ -1,4 +0,0 @@
-cd C:\Users\PC12\Dropbox
-forfiles /D -14 /C "cmd /c rmdir /s /q @file"
-start https://docs.google.com/spreadsheets/d/1vMeYMA-GLuTP_o0pUCwe9oUxSEoXZIXseeiYiiARObg/edit#gid=0
-start https://luftaquila.github.io/ajoumaker/
